94304d4648cfe2df5fdc28cb25987c050afa24c0
Satarupa Pati <satarupapati15@gmail.com>
2025-11-21 10:56:39 +0530

    Delete Day_16 directory

--- Day_16/Ques2.sql
+++
@@ -1,5 +0,0 @@
--- List staff who work in services that had any week with patient satisfaction below 70.
-SELECT DISTINCT s.staff_id, s.staff_name, s.service
-FROM staff s
-JOIN services_weekly w ON s.service=w.service
-WHERE w.patient_satisfaction < 70; 
--- Day_16/Ques3.sql
+++
@@ -1,9 +0,0 @@
--- Show patients from services where total admitted patients exceed 1000.
-SELECT p.patient_id, p.name, p.service
-FROM patients p
-WHERE p.service IN (
-SELECT service
-FROM services_weekly
-GROUP BY service
-HAVING SUM(patients_admitted) > 1000
-);
